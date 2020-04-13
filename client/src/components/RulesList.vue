deec37840ad557dbd596d1325e59a1ac2f67ecd3
fierytortilla <dschlessinger@live.com>
2020-04-13 14:17:02 +0100

    started setting up a rules list display

---
+++ client/src/components/RulesList.vue
@@ -0,0 +1,45 @@
+<template>
+<div>
+
+</div>
+</template>
+
+<script>
+import Scorecard from '../models/scorecard.js';
+import {eventBus} from '../main.js'
+
+export default {
+    name: 'rules-list',
+    data(){
+        return {
+            scoreCard= null
+        }
+    },
+    mounted(){
+      this.getScoreCard(),
+      event
+    },
+    methods: {
+      getScoreCard(){
+        this.scoreCard= new Scorecard
+      },
+      displayRules(){
+          rules = [];
+          for(row in this.scoreCard.upper.scores){
+              rules.push(row.scoringRule)
+          }
+          for(row in this.scoreCard.lower.scores){
+              rules.push(row.scoringRule)
+          }
+          return rules;
+      }
+    }
+    
+
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ client/src/components/RulesListItem.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+    <li v-on:click="handleClick"><rules-list /></li>
+</div>
+</template>
+
+<script>
+import RulesList from '../components/RulesList.vue'
+import {eventBus} from '../main.js'
+
+export default {
+    name: 'rules-list-item',
+    components: {
+        "rules-list": RulesList
+    },
+    methods: {
+        handleClick(){
+            eventBus.$emit('display-rules', null)
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
