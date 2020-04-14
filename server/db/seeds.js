7be7af41661356bff4ff611220cfcd49f701faca
michaeld55 <michaeld55@hotmail.co.uk>
2020-04-14 13:21:31 +0100

    changed seeds.js to match how data is saved and displayed

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -3,18 +3,18 @@ db.dropDatabase();
 
 db.scores.insertMany([
     {
-        "userName": "Michael",
-        "scoreNumber": 1502,
+        "playerName": "Michael",
+        "highScore": 1502,
         "date": "2020-02-20"
     },
     {
-        "userName": "Mairi",
-        "scoreNumber": 1700,
+        "playerName": "Mairi",
+        "highScore": 1700,
         "date": "2020-02-20"
     },
     {
-        "userName": "Keith",
-        "scoreNumber": 1458,
+        "playerName": "Keith",
+        "highScore": 1458,
         "date": "2020-02-20"
     }
 ])
