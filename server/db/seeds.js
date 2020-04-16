a08d53f702873cdebcb7c705acc68f23e12c52b3
michaeld55 <michaeld55@hotmail.co.uk>
2020-04-16 11:38:20 +0100

    added line padding

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -4,17 +4,17 @@ db.dropDatabase();
 db.scores.insertMany([
     {
         "playerName": "Michael",
-        "highScore": 1502,
+        "highScore": 225,
         "date": "2020-02-20"
     },
     {
         "playerName": "Mairi",
-        "highScore": 1700,
+        "highScore": 90,
         "date": "2020-02-20"
     },
     {
-        "playerName": "Keith",
-        "highScore": 1458,
+        "playerName": "David",
+        "highScore": 150,
         "date": "2020-02-20"
     }
 ])
