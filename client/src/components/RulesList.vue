caae4105676b78edd54de556e2fb8a62ad4f7db3
fierytortilla <dschlessinger@live.com>
2020-04-16 10:14:50 +0100

    added description of rules

--- client/src/components/RulesList.vue
+++ client/src/components/RulesList.vue
@@ -1,9 +1,14 @@
 <template lang='html'>
-<div id="rules-list-container" v-if="rules.length">  
+<div id="rules-list-container" v-if="rules.length">
+    <p>The point of the game is to acquire as many points as possible by rolling five dice. The face values of the rolled dice are combined to make certain kinds of scores, as described in table below.  
+        Per one turn, a user may roll the dice at maximum three times. Then they must select a score they want to obtain on the scorecard, which signals the end of the turn. A user can also choose to save some dice and roll others after the first and second roll in order to try achieving certain scores.
+        Users can also select a score after the first roll.
+        After a score is selected, it cannot be selected again. Once all scores have been selected on the scorecard, the game is over.
+    </p>  
     <table style="width:100%">
         <tr>
-            <th>Score</th>
-            <th>Score Description</th> 
+            <th>Score Name</th>
+            <th>Description</th> 
         </tr>
         <tr v-for="rule in rules">
             <td>{{Object.keys(rule)[0]}}:</td>
